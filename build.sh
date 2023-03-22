dcdce5bcf67c402e9fad5d678e9aa86f8076549f
n0kovo <n0kovo@riseup.net>
2023-03-22 11:22:18 +0100

    Revert "Lunar"
    This reverts commit b3ca327e19ce7d7f21c54755c0f71ca776e667f6.

--- build.sh
+++ build.sh
@@ -28,7 +28,7 @@ apt-get install -y live-build patch binutils zstd
 dpkg -i ./debs/*.deb
 patch /usr/lib/live/build/binary_grub-efi < live-build-fix-shim-remove.patch
 
-ln -sfn /usr/share/debootstrap/scripts/gutsy /usr/share/debootstrap/scripts/lunar
+ln -sfn /usr/share/debootstrap/scripts/gutsy /usr/share/debootstrap/scripts/impish
 
 build () {
   BUILD_ARCH="$1"
